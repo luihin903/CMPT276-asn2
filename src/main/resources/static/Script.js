dfe3c0f707cbcb82f9dc37eb1c1d5a24dca494bc
luihin903 <luihin903@gmail.com>
2023-06-11 16:57:02 -0700

    centered index.html

--- src/main/resources/static/Script.js
+++ src/main/resources/static/Script.js
@@ -1,4 +1,8 @@
-var width = window.innerWidth;
-
-console.log(width);
-
+var inter = window.setInterval(function() {
+    var width = window.innerWidth;
+    console.log(width)
+    var elements = document.getElementsByClassName("center")
+    for (var i = 0; elements.length; i ++) {
+        elements[i].style.width = width.toString() + "px"
+    }
+}, 100)
